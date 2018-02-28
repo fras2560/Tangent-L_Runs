1d68bf1524ffaffa3e8a86f988b2c6078a059417
fras2560 <dallas_j_frase@hotmail.com>
2018-02-27 20:46:03 -0500

    Adding the results of the math weight experiment

---
+++ scripts/evaluateTrecEvalOnFolder.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+TREC_EVAL_PATH="/home/d6fraser/git/trec_eval"
+evaluate_folder(){
+	for f in *; do
+		if [ ! -d "$file" ];
+		then
+      		$TREC_EVAL_PATH/./trec_eval "-q" $3 $1/$f  > $2/$4$f
+			echo "Processing $f file"
+      	fi
+	done 
+}
+FILES=/path/to/*
+parentdir="$(dirname "$PWD")"
+workingDir="$parentdir/runs"
+outputDir="$parentdir/trec_eval_output"
+judgementDir="$parentdir/judgements"
+cd $workingDir
+for d in * ;
+do
+	if [[ -d $d ]]; then
+		# make the output directory
+		mkdir "$outputDir/$d"
+		cd $d
+		echo "$workingdir/$d"
+		echo "$outputDir/$d"
+		if [ $d == "evaluatingMathFeatures" ];
+		then
+			evaluate_folder "$workingDir/$d" "$outputDir/$d" "$judgementDir/NTCIR12-MathWiki-main_Relevant.dat" "Relevant"
+			evaluate_folder "$workingDir/$d" "$outputDir/$d" "$judgementDir/NTCIR12-MathWiki-main_PartiallyRelevant.dat" "PartiallyRelevant"
+		elif [ $d == "evaluatingMathWeight" ];
+		then
+			evaluate_folder "$workingDir/$d" "$outputDir/$d" "$judgementDir/NTCIR12-MathWiki-main_Relevant.dat" "Relevant"
+			evaluate_folder "$workingDir/$d" "$outputDir/$d" "$judgementDir/NTCIR12-MathWiki-main_PartiallyRelevant.dat" "PartiallyRelevant"
+		elif [ $d == "evaluatingProximity" ];
+		then
+			evaluate_folder "$workingDir/$d" "$outputDir/$d" "$judgementDir/NTCIR12-MathWiki-main_Relevant.dat" "Relevant"
+			evaluate_folder "$workingDir/$d" "$outputDir/$d" "$judgementDir/NTCIR12-MathWiki-main_PartiallyRelevant.dat" "PartiallyRelevant"
+		elif [ $d == "evaluatingRankingFunctions" ];
+		then
+			evaluate_folder "$workingDir/$d" "$outputDir/$d" "$judgementDir/NTCIR12-MathWiki-main_Relevant.dat" "Relevant"
+			evaluate_folder "$workingDir/$d" "$outputDir/$d" "$judgementDir/NTCIR12-MathWiki-main_PartiallyRelevant.dat" "PartiallyRelevant"
+		fi 
+		echo "Creating output directory: $outputDir/$d"
+		
+		
+		
+		cd $workingDir
+	fi
+  echo "Directory: $d "
+  # take action on each file. $f store current file name
+done
